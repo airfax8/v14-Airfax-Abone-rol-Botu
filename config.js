2f4d25b1adc2e1ce0757303a50f24204bb13eaab
Airfax <115174124+airfax8@users.noreply.github.com>
2023-04-12 18:15:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 module.exports = {
-    prefix : "!",
-    sahip : "837953203197968414",
-    token : "MTA0NzU0ODA4NzUxNTc0MjI2OA.GK-XBr.gYYq__yIjPiTAMHUdkKD5JDPaGJtm26vnvwssw",
-    durum : "AirCod #2023",
-    basvuruYt: "1008412421687357572",
-    yetkiRolleri: "1036015808989626398",
-    guildID: "1008322912891195393",
-    Footer: "AirCod #2023",
-
+    prefix : "",
+    sahip : "",
+    token : "",
+    durum : "",
   }
